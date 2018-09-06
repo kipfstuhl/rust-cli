6575562e48e5c6b4851631c9bdbbeb7590b06fde
Jonas Kipfstuhl <jonas.kipfstuhl@t-online.de>
2018-09-06 23:52:03 +0200

    add some colour
    Add code to get colors in ANSI temrinals. Only works when
    tput (curses) is available.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use rand::{thread_rng, Rng};
 use std::io::{self, Write};
 use std::{thread, time};
 
+use std::process::Command;
+
 fn main() {
     let output = String::from("Hello, world!");
     println!("{}", output);
@@ -15,6 +17,43 @@ fn main() {
         .expect("panicking");
     print_slice_with_random_delay(&output[0..5], time::Duration::from_millis(300), Some(30))
         .expect("panicking");
+
+    // try some color
+    // does not work in Emacs shells, except term (it is ANSI
+    // compliant), so usual execution via cargo mode commands will not
+    // fully work
+    let cmd = Command::new("tput")
+        .args(&["setaf", "4"])
+        .output()
+        .expect("failed to execute tput");
+    let color_code = String::from_utf8_lossy(&cmd.stdout);
+
+    let cmd = Command::new("tput")
+        .args(&["bold"])
+        .output()
+        .expect("failed to execute tput");
+    let bold_code = String::from_utf8_lossy(&cmd.stdout);
+
+    let cmd = Command::new("tput")
+        .args(&["sgr0"])
+        .output()
+        .expect("failed to execute tput");
+    let reset_code = String::from_utf8_lossy(&cmd.stdout);
+
+    println!("Status: {}", cmd.status);
+    println!("Output: {}", color_code);
+    println!("should be blue");
+    print!("{}", reset_code);
+
+    println!("Status: {}", cmd.status);
+    println!("Output: {}", bold_code);
+    print!("{}", color_code);
+    println!("should be bold and blue");
+    print!("{}", reset_code);
+
+    println!("Status: {}", cmd.status);
+    println!("Output: {}", reset_code);
+    println!("should be normal text again");
 }
 
 pub fn print_string_with_delay(word: &String, delay: time::Duration) -> io::Result<()> {
