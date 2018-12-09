7efc97e1b1b37269c06081becd60e7bdeac43912
Jonas Kipfstuhl <jonas.kipfstuhl@t-online.de>
2018-12-09 17:16:04 +0100

    improve progress.rs
    draw the bar at the beginning, without anything done, then increase it
    everytime something is written.
    
    also some numbers are adjusted, so now it should be right with jumping
    around the lines.

--- src/bin/progress.rs
+++ src/bin/progress.rs
@@ -32,25 +32,26 @@ fn main() {
     println!("{}", output);
     println!("Now some default output, not respecting user defined delay.");
     let bar = ProgressBar::new(4);
+    bar.tick();
 
     print_with_delay(&output, Duration::from_millis(100), out).expect("panicking");
-    //    out.move_cursor_up(1);
+    out.move_cursor_up(1).unwrap();
     bar.inc(1);
     out.move_cursor_down(1).unwrap();
     print_with_random_delay(&output, Duration::from_millis(200), None, out).expect("panicking");
-    out.move_cursor_up(1).unwrap();
+    out.move_cursor_up(2).unwrap();
     bar.inc(1);
     out.move_cursor_down(2).unwrap();
     print_with_random_delay(&output, Duration::from_millis(200), Some(100), out)
         .expect("panicking");
-    out.move_cursor_up(2).unwrap();
+    out.move_cursor_up(3).unwrap();
     bar.inc(1);
     out.move_cursor_down(3).unwrap();
 
     println!("Now respect the user specified delay, or use the default value.");
     let duration = Duration::from_millis(args.delay);
     print_with_delay(&output, duration, out).expect("panicking");
-    out.move_cursor_up(4).unwrap();
+    out.move_cursor_up(5).unwrap();
     bar.inc(1);
     bar.finish();
     out.move_cursor_down(5).unwrap();
