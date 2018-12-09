7af237853216db89181b37123a6f723adb3e1f3e
Jonas Kipfstuhl <jonas.kipfstuhl@t-online.de>
2018-12-09 22:06:53 +0100

    add macro to the progessbar example
    Add a macro for the cursor movin, then updating the progress bar,
    afterwards move cursor down. That makes it easy to be lazy, this is
    what programming is for.

--- src/bin/progress.rs
+++ src/bin/progress.rs
@@ -4,6 +4,14 @@ use std::time::Duration;
 use structopt::StructOpt;
 use versuch::{print_with_delay, print_with_random_delay};
 
+macro_rules! at_line {
+    ($writer:ident, $n:expr ,$x:stmt) => {
+        $writer.move_cursor_up($n).unwrap();
+        $x;
+        $writer.move_cursor_down($n).unwrap();
+    };
+}
+
 #[derive(Debug, StructOpt)]
 #[structopt(
     name = "",
@@ -35,24 +43,24 @@ fn main() {
     bar.tick();
 
     print_with_delay(&output, Duration::from_millis(100), out).expect("panicking");
-    out.move_cursor_up(1).unwrap();
-    bar.inc(1);
-    out.move_cursor_down(1).unwrap();
+
+    at_line!(out, 1, bar.inc(1));
+
     print_with_random_delay(&output, Duration::from_millis(200), None, out).expect("panicking");
-    out.move_cursor_up(2).unwrap();
-    bar.inc(1);
-    out.move_cursor_down(2).unwrap();
+
+    at_line!(out, 2, bar.inc(1));
+
     print_with_random_delay(&output, Duration::from_millis(200), Some(100), out)
         .expect("panicking");
-    out.move_cursor_up(3).unwrap();
-    bar.inc(1);
-    out.move_cursor_down(3).unwrap();
+
+    at_line!(out, 3, bar.inc(1));
 
     println!("Now respect the user specified delay, or use the default value.");
     let duration = Duration::from_millis(args.delay);
     print_with_delay(&output, duration, out).expect("panicking");
-    out.move_cursor_up(5).unwrap();
-    bar.inc(1);
-    bar.finish();
-    out.move_cursor_down(5).unwrap();
+
+    at_line!(out, 5, {
+        bar.inc(1);
+        bar.finish();
+    });
 }
